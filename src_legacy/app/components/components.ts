3e6f6705989afc43659f64062887fd83beccc106
Patrick Roza <roza@sixnetworks.net>
2016-06-14 09:45:23 +0200

    Cleanup legacy modules

--- src_legacy/app/components/components.ts
+++ src_legacy/app/components/components.ts
@@ -1,28 +1,28 @@
 ﻿module MyApp.Components {
-    class ComponentsModule extends Tk.Module {
-        static $name = "ComponentsModule";
+  class ComponentsModule extends Tk.Module {
+    static $name = "ComponentsModule";
 
-        constructor() {
-            super('Components', ['commangular']);
-            this.app
-                .config(['$commangularProvider', $commangularProvider => registerCommands(this.commands, $commangularProvider)])
-                .factory('refreshService', [
-                    () => {
-                        var cache = {};
-                        return {
-                            getType: (type) => cache[type],
-                            refreshType: (type) => cache[type] = !cache[type]
-                        };
-                    }
-                ]);
-        }
+    constructor() {
+      super('Components', ['commangular']);
+      this.app
+        .config(['$commangularProvider', $commangularProvider => registerCommands(this.commands, $commangularProvider)])
+        .factory('refreshService', [
+          () => {
+            var cache = {};
+            return {
+              getType: (type) => cache[type],
+              refreshType: (type) => cache[type] = !cache[type]
+            };
+          }
+        ]);
     }
+  }
 
-    export function registerCQ(command) { app.registerCommand(command); }
+  export function registerCQ(command) { app.registerCommand(command); }
 
-    export function registerService(service) { app.app.service(service.$name, service); }
+  export function registerService(service) { app.app.service(service.$name, service); }
 
-    export function registerController(controller) { app.app.controller(controller.$name, controller); }
+  export function registerController(controller) { app.app.controller(controller.$name, controller); }
 
-    var app = new ComponentsModule();
+  var app = new ComponentsModule();
 }
