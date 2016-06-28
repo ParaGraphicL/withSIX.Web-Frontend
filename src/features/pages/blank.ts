b2871db5c1e83e82bbaba3767c52ffdbafc93185
Patrick Roza <roza@sixnetworks.net>
2016-06-28 19:53:58 +0200

    Angularbs

--- src/features/pages/blank.ts
+++ src/features/pages/blank.ts
@@ -1,2 +1,21 @@
+import {activationStrategy} from 'aurelia-router';
 import {MainBase} from './index';
-export class Blank extends MainBase { }
+export class Blank extends MainBase {
+  activate() {
+    this.tools.Debug.log("AURELIA: angular vm loaded");
+    // This works around the issue of routing for Angular while Aurelia is involved..angular
+    // TODO: Better workaround than the rootscope apply?
+    this.notifyAngular();
+  }
+
+  deactivate() {
+    this.tools.Debug.log("AURELIA: angular vm unloaded");
+    // This works around the issue of routing for Angular while Aurelia is involved..angular
+    // TODO: Better workaround than the rootscope apply?
+    this.notifyAngular();
+  }
+
+  determineActivationStrategy() {
+    return activationStrategy.replace;
+  }
+}
