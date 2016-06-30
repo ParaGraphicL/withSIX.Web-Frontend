9ec1a61fbcc7578c19086cfca12f3a097f8431de
Patrick Roza <roza@sixnetworks.net>
2016-06-30 09:59:53 +0200

    Use actual game states

--- src/features/games/games.ts
+++ src/features/games/games.ts
@@ -1,21 +1,46 @@
-import {ViewModel, Query, DbQuery, handlerFor, breeze} from '../../framework'
+import {ViewModel, Query, DbClientQuery, handlerFor, breeze, ItemState, IGame, IBreezeGame} from '../../framework'
 export class Games extends ViewModel {
-  games: any[];
+  games: IGame[];
   async activate() {
     $('#header-row').attr('style', 'background-image: url("' + this.w6.url.getAssetUrl('img/play.withSIX/header.jpg') + '");');
     $('body').removeClass('game-profile');
     this.games = await new GetGames().handle(this.mediator);
+    this.handleGameStates();
+  }
+
+  async handleGameStates() {
+    let d = await new GetInstalledGames().handle(this.mediator);
+    this.games.forEach(x => {
+      if (d.has(x.id))
+        (<any>x).state = (<any>d[x.id]).state;
+    })
   }
 }
 
-export class GetGames extends Query<any[]> { }
+export class GetGames extends Query<IGame[]> { }
 @handlerFor(GetGames)
-export class GetGamesHandler extends DbQuery<GetGames, any[]> {
-  handle(request: GetGames) {
-    return this.context.executeQuery(breeze.EntityQuery.from("Games")
+export class GetGamesHandler extends DbClientQuery<GetGames, IGame[]> {
+  async handle(request: GetGames): Promise<IGame[]> {
+    let r = await this.context.executeQuery<IBreezeGame>(breeze.EntityQuery.from("Games")
       .where("parentId", breeze.FilterQueryOp.Equals, null)
       .where("public", breeze.FilterQueryOp.Equals, true) // ...
-      .orderBy("name"))
-      .then(data => data.results)
+      .orderBy("name"));
+    return <IGame[]><any>r.results;
+  }
+}
+
+export class GetInstalledGames extends Query<Map<string, IGame>> { }
+
+@handlerFor(GetInstalledGames)
+class GetInstalledGamesHandler extends DbClientQuery<GetInstalledGames, Map<string, IGame>> {
+  public async handle(request: GetInstalledGames): Promise<Map<string, IGame>> {
+    try {
+      if (!this.w6.miniClient.isConnected) throw new Error("client not running");
+      let d: { games: IGame[] } = await this.client.getGames();
+      d.games.forEach(x => (<any>x).state = ItemState.Uptodate)
+      return this.tools.aryToMap(d.games, x => x.id);
+    } catch (err) {
+      return new Map<string, IGame>();
+    }
   }
 }
--- src/features/profile/content/game.ts
+++ src/features/profile/content/game.ts
@@ -1,4 +1,4 @@
-import {MenuItem, ViewModel, uiCommand2, OpenFolder} from '../../../framework';
+import {MenuItem, ViewModel, uiCommand2, OpenFolder, ItemState, IMenuItem} from '../../../framework';
 import {Query, DbClientQuery, handlerFor, VoidCommand, IGame, FolderType} from '../../../framework';
 import {Index as SettingsIndex} from '../../settings/index';
 
@@ -8,30 +8,44 @@ export class Game extends ViewModel {
   url: string;
   public model: IGame;
   icon = "withSIX-icon-Joystick";
-  itemStateClass = 'uptodate';
+  get itemStateClass() { return this.isInstalled ? 'uptodate' : 'notinstalled' };
   openConfigFolder: ICommand<void>;
 
   hasStats: boolean;
 
+  get isInstalled() { return (<any>this).model.state == ItemState.Uptodate }
+
   activate(model: IGame) {
     this.model = model;
     this.hasStats = (<any>model).collectionsCount != null;
+    let isInstalledObservable = this.observeEx(x => x.isInstalled);
     this.subscriptions.subd(d => {
-      d(this.launch);
-      d(this.openFolder);
-      if (this.model.slug.startsWith('Arma')) {
-        d(this.openConfigFolder = uiCommand2("Open config folder", () => new OpenFolder(this.model.id, this.tools.emptyGuid, FolderType.Config).handle(this.mediator), { icon: 'icon withSIX-icon-Folder' }));
-        this.topMenuActions.push(new MenuItem(this.openConfigFolder));
-      }
+      d(this.launch = uiCommand2("Launch", () => new LaunchGame(this.model.id).handle(this.mediator), { icon: "icon withSIX-icon-Hexagon-Play", isVisibleObservable: isInstalledObservable }));
+      d(this.openFolder = uiCommand2("Open folder", () => new OpenFolder(this.model.id).handle(this.mediator), { icon: 'icon withSIX-icon-Folder', isVisibleObservable: isInstalledObservable }));
+      if (this.model.slug.startsWith('Arma'))
+        d(this.openConfigFolder = uiCommand2("Open config folder", () => new OpenFolder(this.model.id, this.tools.emptyGuid, FolderType.Config).handle(this.mediator), { icon: 'icon withSIX-icon-Folder', isVisibleObservable: isInstalledObservable }));
     })
     let base = 'img/play.withSIX/games/' + model.slug;
     this.image = this.w6.url.getAssetUrl(`${base}/logo-overview.png`);
     this.bgUrl = this.w6.url.getAssetUrl(`${base}/headers/header.png`);
     this.url = window.location.pathname == '/p' || window.location.pathname.startsWith("/p/") ? `/p/${model.slug}` : `/me/library/${model.slug}`
+
+    this.bottomActions = [
+      new MenuItem(this.launch)
+    ]
+
+    this.topMenuActions = [
+      new MenuItem(this.launch),
+      new MenuItem(this.openStream),
+      new MenuItem(this.openLibrary),
+      new MenuItem(this.openSettings),
+      new MenuItem(this.openFolder)
+    ]
+    if (this.openConfigFolder) this.topMenuActions.push(new MenuItem(this.openConfigFolder));
   }
 
-  launch = uiCommand2("Launch", () => new LaunchGame(this.model.id).handle(this.mediator), { icon: "icon withSIX-icon-Hexagon-Play" });
-  openFolder = uiCommand2("Open folder", () => new OpenFolder(this.model.id).handle(this.mediator), { icon: 'icon withSIX-icon-Folder' })
+  launch: ICommand<void>;
+  openFolder: ICommand<void>;
   openStream = uiCommand2('Stream', async () => this.navigateInternal(`/p/${this.model.slug}`), { icon: 'icon withSIX-icon-Nav-Stream' });
   openLibrary = uiCommand2('Library', async () => this.navigateInternal(`/me/library/${this.model.slug}`), { icon: 'icon withSIX-icon-Nav-Collection' });
   openSettings = uiCommand2('Settings', async () => {
@@ -39,19 +53,8 @@ export class Game extends ViewModel {
     this.dialog.open({ viewModel: SettingsIndex, model: model })
   }, { icon: 'icon withSIX-icon-Settings' });
 
-
-
-  bottomActions = [
-    new MenuItem(this.launch)
-  ]
-
-  topMenuActions = [
-    new MenuItem(this.launch),
-    new MenuItem(this.openStream),
-    new MenuItem(this.openLibrary),
-    new MenuItem(this.openSettings),
-    new MenuItem(this.openFolder)
-  ]
+  bottomActions: IMenuItem[];
+  topMenuActions: IMenuItem[];
 }
 
 export enum LaunchType {
