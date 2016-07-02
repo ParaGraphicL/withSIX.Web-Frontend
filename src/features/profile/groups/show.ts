d91de91b387c1e6504c945b9bbd353902544956a
Patrick Roza <roza@sixnetworks.net>
2016-07-02 14:20:10 +0200

    Fix group menu visibility

--- src/features/profile/groups/show.ts
+++ src/features/profile/groups/show.ts
@@ -1,4 +1,4 @@
-import {UiContext,ViewModel, Mediator} from '../../../framework'
+import {UiContext, ViewModel, Mediator} from '../../../framework'
 import {inject} from 'aurelia-framework';
 import {Router, RouterConfiguration, RouteConfig} from 'aurelia-router';
 import {EventAggregator} from 'aurelia-event-aggregator';
@@ -24,15 +24,13 @@ export class Show extends ViewModel {
   router: Router;
   configureRouter(config: RouterConfiguration, router: Router) {
     this.router = router;
-    let canSee = this.w6.userInfo.isAdmin || this.w6.userInfo.isManager;
-    let stagingEnv = this.w6.url.environment >= this.tools.Environment.Staging;
     config.map([
-      { route: '', name: 'home', moduleId: 'features/profile/groups/home/index', nav: true, title: 'Home' },
+      { route: '', name: 'home', moduleId: 'features/profile/groups/home/index', nav: true, title: 'Home', settings: { icon: "icon withSIX-icon-Home" } },
       { route: 'join/:token', name: 'join', moduleId: 'features/profile/groups/join', nav: false, title: 'Join' },
       { route: 'members', name: 'members', moduleId: 'features/profile/groups/members/index', nav: true, title: 'Members' },
-      { route: 'collections', name: 'collections', moduleId: 'features/profile/groups/collections/index', nav: true, title: 'Collections' },
-      { route: 'mods', name: 'mods', moduleId: 'features/profile/groups/mods/index', nav: true, title: 'Mods' },
-      { route: 'servers', name: 'servers', moduleId: 'features/profile/groups/servers/index', nav: canSee && stagingEnv, title: 'Servers' }
+      { route: 'collections', name: 'collections', moduleId: 'features/profile/groups/collections/index', nav: true, title: 'Collections', settings: { icon: "icon withSIX-icon-Nav-Collection" } },
+      { route: 'mods', name: 'mods', moduleId: 'features/profile/groups/mods/index', nav: true, title: 'Mods', settings: { icon: "icon withSIX-icon-Nav-Collection" } },
+      { route: 'servers', name: 'servers', moduleId: 'features/profile/groups/servers/index', nav: this.features.groups && this.features.servers, title: 'Servers', settings: { icon: "icon withSIX-icon-Nav-Server" } }
     ])
   }
 }
--- src/features/profile/index.ts
+++ src/features/profile/index.ts
@@ -11,16 +11,15 @@ export class Index extends ViewModel {
   constructor(ui: UiContext) { super(ui); }
   router: Router;
   configureRouter(config: RouterConfiguration, router: Router) {
-    let canSee = this.w6.userInfo.isAdmin || this.w6.userInfo.isManager;
     let modulePath = 'features/profile/';
     config.map([
       { route: ['', 'home'], name: 'home', moduleId: 'features/profile/home/index', nav: this.w6.enableBasket, title: 'Home' },
       { route: 'library', name: 'library', moduleId: 'features/profile/library/index', nav: this.w6.enableBasket, title: 'Library' },
       { route: 'content', name: 'content', moduleId: `${modulePath}content/index`, nav: this.isLoggedIn, title: 'Content', auth: true },
       { route: 'friends', name: 'friends', moduleId: 'features/pages/angular', nav: this.isLoggedIn, title: 'Friends', auth: true },
-      { route: 'groups', name: 'groups', moduleId: 'features/profile/groups/index', nav: canSee || this.w6.userInfo.isManager, title: 'Groups', auth: true },
+      { route: 'groups', name: 'groups', moduleId: 'features/profile/groups/index', nav: this.features.groups || this.w6.userInfo.isManager, title: 'Groups', auth: true },
       { route: 'messages', name: 'messages', moduleId: 'features/pages/angular', nav: this.isLoggedIn, title: 'Messages', auth: true },
-      { route: 'blog', name: 'blog', moduleId: 'features/pages/angular', nav: this.w6.userInfo.id && canSee ? true : false, auth: true, title: 'Blog' },
+      { route: 'blog', name: 'blog', moduleId: 'features/pages/angular', nav: this.w6.userInfo.id && (this.w6.userInfo.isAdmin || this.w6.userInfo.isManager) ? true : false, auth: true, title: 'Blog' },
       { route: 'settings', name: 'settings', moduleId: 'features/pages/angular', nav: this.isLoggedIn, title: 'Settings', auth: true, settings: { cls: 'pull-right' } }
     ]);
 
