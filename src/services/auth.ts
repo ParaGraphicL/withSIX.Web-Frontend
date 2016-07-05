c3e057da066d9537a1cffcb58d4d6563cb32b8e8
Patrick Roza <roza@sixnetworks.net>
2016-07-05 10:05:26 +0200

    Improve login process

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -25,18 +25,35 @@ export class Login extends LoginBase {
       var response = await this.authService.authenticate('localIdentityServer');
       window.localStorage[LoginBase.refreshToken] = response.content.refresh_token;
       window.localStorage[LoginBase.idToken] = response.content.id_token;
-      this.ls.set('w6.event', { name: 'login', data: null }); // TODO: Include user id so we can switch logged in accts if needed?
       if (console && console.log) console.log("$$$$ Succesfully loggedin")
+      this.ls.set('w6.event', { name: 'login', data: null }); // TODO: Include user id so we can switch logged in accts if needed?
       this.redirect(url);
-      return;
     } catch (err) {
-      if (err && err.data == "Provider Popup Blocked")
-        // TODO: Reconfigure without popup but redirect?
-        if (await this.ui.toastr.error("Popup blocked, please allow the login popup in your browser", "Failed to login")) await this.login(pathAndSearch);
-        else
-          this.ui.toastr.error("Unknown error", "Failed to login");
-      throw err;
+      await this.handleLoginError(err, pathAndSearch);
     }
+  }
+
+  async handleLoginError(err, pathAndSearch?) {
+    if (!err) throw err;
+    if (err.data) {
+      if (err.data == "Provider Popup Blocked") {
+      // TODO: Reconfigure without popup but redirect?
+      if (await this.ui.toastr.error("Popup blocked, please allow withSIX popups, and click here to try again", "Login failure", { timeOut: 0 })) {
+        await this.login(pathAndSearch);
+        return;
+      } else this.ui.toastr.error("Please allow withSIX popups and try again", "Login failure");
+    } else if (err.data == "Problem poll popup") {
+      if (await this.ui.toastr.error(`The popup was closed without logging in succesfully. Click here to try again`, "Login failure", { timeOut: 0 })) {
+        await this.login(pathAndSearch);
+        return;
+      }
+    } else this.ui.toastr.error(`Unknown error: ${err.data}`, "Login failure")
+  } else if (err.error) this.ui.toastr.error(`Unknown error: ${err.error}`, "Login failure")
+  else {
+      if (err instanceof Error) this.ui.toastr.error(`Unknown error: ${err}`, "Login failure")
+      else this.ui.toastr.error(`Unknown error`, "Login failure")
+    }
+    throw err;
   }
 
   logout() {
