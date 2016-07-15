3e499113707e372042a7c2b6a61cf335d089517c
Patrick Roza <roza@sixnetworks.net>
2016-07-15 16:32:40 +0200

    fix build:prod

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,7 +80,7 @@ module.exports = function(grunt) {
         command: 'gulp prepare-release'
       },
       prod: {
-        command: 'npm run prod'
+        command: 'npm run build:prod'
       }
     },
 
