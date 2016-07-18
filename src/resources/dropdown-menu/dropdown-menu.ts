e38f034d2d73067a43cbddf342411cff6a7ba95f
Patrick Roza <roza@sixnetworks.net>
2016-07-18 11:48:20 +0200

    Separate adjust for weird issue with the dropdown on the mod page

--- src/resources/dropdown-menu/dropdown-menu.ts
+++ src/resources/dropdown-menu/dropdown-menu.ts
@@ -19,6 +19,8 @@ export class DropdownMenu extends ViewModel {
   @bindable hideWhenEmpty = true;
   @bindable menuCls: string;
   @bindable itemCls: string;
+  @bindable horAdjust: number;
+  @bindable verAdjust: number;
   isVisible: boolean;
   menu: Element;
   menuButton: Element;
@@ -42,7 +44,7 @@ export class DropdownMenu extends ViewModel {
     this.menuTrgr = $(this.menuButton);
     this.menuEl = $(this.menu);
     (<any>this.menuTrgr).contextMenu('popup', this.menuEl, { // 'menu' (is more menu styled)
-      displayAround: 'trigger', position: this.direction, horAdjust: this.direction === 'left' ? -104 : undefined,
+      displayAround: 'trigger', position: this.direction, horAdjust: this.horAdjust, verAdjust: this.verAdjust,
       // On open, let's rid ourselves from any parent styling/behavior, and restore once we close
       onOpen: () => { this.menuEl.appendTo('body'); this.open = true; },
       onClose: () => { this.menuEl.appendTo(this.element); this.open = false; }
