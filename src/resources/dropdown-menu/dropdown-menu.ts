1f37b75fde220a7bf330f3ce94ae2077e6be6b6c
Patrick Roza <roza@sixnetworks.net>
2016-07-18 11:32:13 +0200

    Improve menu direction support

--- src/resources/dropdown-menu/dropdown-menu.ts
+++ src/resources/dropdown-menu/dropdown-menu.ts
@@ -15,7 +15,7 @@ export class DropdownMenu extends ViewModel {
   @bindable header: string;
   @bindable icon: string = "withSIX-icon-Share-Dots-V";
   @bindable textCls: string;
-  @bindable direction: string = "down";
+  @bindable direction: string = "auto";
   @bindable hideWhenEmpty = true;
   @bindable menuCls: string;
   @bindable itemCls: string;
@@ -42,7 +42,7 @@ export class DropdownMenu extends ViewModel {
     this.menuTrgr = $(this.menuButton);
     this.menuEl = $(this.menu);
     (<any>this.menuTrgr).contextMenu('popup', this.menuEl, { // 'menu' (is more menu styled)
-      displayAround: 'trigger',
+      displayAround: 'trigger', position: this.direction, horAdjust: this.direction === 'left' ? -104 : undefined,
       // On open, let's rid ourselves from any parent styling/behavior, and restore once we close
       onOpen: () => { this.menuEl.appendTo('body'); this.open = true; },
       onClose: () => { this.menuEl.appendTo(this.element); this.open = false; }
