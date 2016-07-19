e55a3a678295d0e101f967985ddf72337bda2d3c
Patrick Roza <roza@sixnetworks.net>
2016-07-19 22:58:25 +0200

    Add more tags support
    quote, size, color, s and center.
    and img source optionally as attr.

--- src/helpers/utils/string.ts
+++ src/helpers/utils/string.ts
@@ -5,32 +5,39 @@ export const endsWithIgnoreCase = (str: string, suffix: string) => str.toLowerCa
 export const startsWithIgnoreCase = (str: string, prefix) => str.toLowerCase().startsWith(prefix.toLowerCase())
 export const indexOfIgnoreCase = (str: string, prefix: string) => str.toLowerCase().indexOf(prefix.toLowerCase())
 export const toUpperCaseFirst = (str: string) => str.split(" ").map(i => i ? i[0].toUpperCase() + i.substring(1) : i).join(" ")
-export const toLowerCaseFirst = (str: string) => str.split(" ").map(i => i ? i[0].toLowerCase() + i.substring(1): i).join(" ")
+export const toLowerCaseFirst = (str: string) => str.split(" ").map(i => i ? i[0].toLowerCase() + i.substring(1) : i).join(" ")
 export const containsIgnoreCase = (str: string, prefix: string) => str.toLowerCase().includes(prefix)
 export const equalsIgnoreCase = (str: string, other: string) => str.toLowerCase() === other.toLowerCase()
 export const truncate = (str: string, count: number) => { if (str.length <= count) return str; return str.substring(0, count) + '..' }
 
 interface Sanitizer {
   (html: string, overrides?): string;
-  defaults: {allowedTags: string[]}
+  defaults: { allowedTags: string[] }
 }
 const sanitizeHtmlLib: Sanitizer = <any>require('sanitize-html');
 
 export const sanitizeHtml = (html: string, overrides?) => sanitizeHtmlLib(html, Object.assign({}, {
-  allowedTags: sanitizeHtmlLib.defaults.allowedTags.concat([ 'img' ])
+  allowedTags: sanitizeHtmlLib.defaults.allowedTags.concat(['img'])
 }, overrides))
 
 import {BBTag} from './bbcode/bbTag';
 import {BBCodeParser} from './bbcode/bbCodeParser';
 
-var li = new BBTag("*", true, false, false, (tag, content, attr) => {
-  return `<li>${content}</li>`;
-}); li.noEndTag = true;
-var bbTags = [BBTag.createSimpleTag("h1"), BBTag.createSimpleTag("h2"), BBTag.createSimpleTag("h3"),
-  new BBTag("list", true, false, false, (tag, content, attr) => {
-    return `<ul>${content}</ul>`;
-}), li];
-var parser =  new BBCodeParser(bbTags.concat(BBCodeParser.defaultTags()));
+var li = BBTag.createSimpleTag("*", true, 'li');
+li.noEndTag = true;
+
+// TODO: Broken markup could be auto-terminated on the first space?
+var bbTags = [
+  BBTag.createSimpleTag("h1"), BBTag.createSimpleTag("h2"), BBTag.createSimpleTag("h3"),
+  BBTag.createSimpleTag("center"),
+  new BBTag("img", true, false, false, (tag, content, attr) => `<img src="${attr["img"] || content}" />`),
+  new BBTag('quote', true, false, false, (tag, content, attr) => `<blockquote ${attr["quote"] ? ` cite="${attr["quote"]}"` : ''}>${content}</blockquote>`),
+  new BBTag('s', true, false, false, (tag, content, attr) => `<span style="text-decoration: line-through">${content}</span>`),
+  new BBTag('size', true, false, false, (tag, content, attr) => `<span style="font-size: ${attr['size'] || '1.5em'}">${content}</span>`),
+  new BBTag('color', true, false, false, (tag, content, attr) => `<span style="color: ${attr['color'] || 'black'}">${content}</span>`),
+  BBTag.createSimpleTag("list", true, 'ul'), li
+];
+const parser = new BBCodeParser(bbTags.concat(BBCodeParser.defaultTags()));
 export const parseBBCode = (bbCode: string) => sanitizeHtml(parser.parseString(bbCode));
 
 // TODO: This is not as good as the C# version we use!
@@ -39,11 +46,11 @@ export const sluggifyEntityName = (str: string) => {
   str = str.replace(r, match => {
     switch (match) {
       case "'":
-          return "";
+        return "";
       case "+":
-          return "plus";
+        return "plus";
       default:
-          return "-";
+        return "-";
     }
   }).trim();
   str = str.replace(r2, "");
