ca5ce8cb16d8565d37b9dfd8f285e0158f2c4acc
Patrick Roza <roza@sixnetworks.net>
2016-08-05 15:05:42 +0200

    Tab fix with minor reduction in experience

--- src/resources/tab-view/tab-view-render.ts
+++ src/resources/tab-view/tab-view-render.ts
@@ -7,21 +7,18 @@ export class TabViewRender extends ViewModel {
   @bindable({ defaultBindingMode: bindingMode.twoWay }) closeOnClick = true;
   @bindable close;
   tabStatus: string;
-  lastActiveTab: ITab;
 
-  get isDropdown() { return this.lastActiveTab && this.lastActiveTab.type == 'dropdown' }
-  get isTab() { return this.lastActiveTab && this.lastActiveTab.type != 'dropdown' }
+  get isDropdown() { return this.selectedTab && this.selectedTab.type == 'dropdown' }
+  get isTab() { return this.selectedTab && this.selectedTab.type != 'dropdown' }
 
   constructor(private el: Element, ui: UiContext) { super(ui); }
 
   bind() {
     this.subscriptions.subd(d => {
-      d(this.whenAnyValue(x => x.selectedTab).filter(x => x != null).subscribe(x => this.lastActiveTab = x));
-      d(this.whenAnyValue(x => x.selectedTab).filter(x => x == null).delay(1000).map(x => this.selectedTab == null).subscribe(x => this.lastActiveTab = null));
       d(this.whenAnyValue(x => x.selectedTab).delay(100).subscribe(x => this.tabStatus = x ? 'active' : 'inactive'));
     })
     ViewModel.setupDelegateEl(this.el, () => this.selectedTab = null);
   }
 
-  get selectedTabName() { return this.lastActiveTab ? (this.lastActiveTab.name || this.lastActiveTab.header.toLowerCase()) : null; }
+  get selectedTabName() { return this.selectedTab ? (this.selectedTab.name || this.selectedTab.header.toLowerCase()) : null; }
 }
