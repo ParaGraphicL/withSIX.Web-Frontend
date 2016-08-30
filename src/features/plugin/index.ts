19e87ed70f9bf30bc3aa26e434a70743c152ac25
Patrick Roza <roza@sixnetworks.net>
2016-08-30 10:55:02 +0200

    m

--- src/features/plugin/index.ts
+++ src/features/plugin/index.ts
@@ -16,7 +16,7 @@ export class Index {
 
     listener = (e: MessageEvent) => {
         if (e.origin === this.parentOrigin) console.log("Received message from plugin: ", e);
-        else console.log("Received message from unknown origin: ", e);
+        else console.warn("Received message from unknown origin: ", e);
     }
 
     send = data => window.parent.postMessage(data, this.parentOrigin)
