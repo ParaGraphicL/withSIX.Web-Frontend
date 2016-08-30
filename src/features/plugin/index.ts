544f6f38882356b582128d2cceb463e53828ee8e
Patrick Roza <roza@sixnetworks.net>
2016-08-30 10:48:08 +0200

    Add check

--- src/features/plugin/index.ts
+++ src/features/plugin/index.ts
@@ -1,13 +1,19 @@
 export class Index {
+    parentOrigin: string;
     activate() {
+        this.parentOrigin = location.search.substring(5);
         window.addEventListener('message', this.listener, false);
+        this.send("Hello from withSIX.com!");
     }
 
     deactivate() {
         window.removeEventListener('message', this.listener);
     }
 
-    listener = function(e: MessageEvent) {
-        console.log("Received message: ", e);
+    listener = (e: MessageEvent) => {
+        if (e.origin === this.parentOrigin) console.log("Received message from plugin: ", e);
+        else console.log("Received message from unknown origin: ", e);
     }
+
+    send = data => window.parent.postMessage(data, this.parentOrigin)
 }
