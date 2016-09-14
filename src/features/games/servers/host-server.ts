31429144e90a04a2b8f3722c8e2e1d94c6f81ff1
Patrick Roza <roza@sixnetworks.net>
2016-09-14 11:24:50 +0200

    Awesome host-server dialog

--- src/features/games/servers/host-server.ts
+++ src/features/games/servers/host-server.ts
@@ -1,13 +1,59 @@
+import { Dialog, uiCommand2, Rx, IReactiveCommand, IDisposable, BusySignalCombiner } from '../../../framework';
+const { Subject } = Rx;
+
 interface IModel {
     scope: string;
+    name: string;
+    password: string;
+    description: string;
+    commsUrl: string;
+    homepageUrl: string;
+    files: FileList;
+   
+    launch: Function;
+    launchDedicated: Function;
 }
 
-export class HostServer {
+export class HostServer extends Dialog<IModel> {
     scopes = ["Public", "Unlisted", "Private"]
-    model;
-    activate() {
-        this.model = {
-            scope: "Public"
-        }
+    activate(model: IModel) {
+        super.activate(Object.assign({
+            scope: "Public",
+            name: `${this.w6.userInfo.displayName}'s server`,
+            password: null,
+            description: null,
+            commsUrl: null,
+            homepageUrl: null,
+            files: null
+        }, model))
+
+        this.subscriptions.subd(d => {
+            const changedObs = this.listFactory.getObserveAll(this.model).map(x => true)
+            d(this.toProperty(changedObs, x => x.changed))
+            d(this.cancel)
+            const busyHandler = new BusySignalCombiner()
+            d(this.launch = uiCommand2('Launch Server', this.performLaunch, { cls: "ok", isVisibleObservable: this.whenAny(x => x.model.launch).map(x => x != null), canExecuteObservable: busyHandler.signal.map(x => !x) }))
+            d(this.launchDedicated = uiCommand2('Launch Dedicated Server', this.performLaunchDedicated, { cls: "ok", isVisibleObservable: this.whenAny(x => x.model.launchDedicated).map(x => x != null), canExecuteObservable: busyHandler.signal.map(x => !x) }))
+            d(busyHandler.subscribe(this.launch, this.launchDedicated))
+        })
     }
+    performCancel = async () => {
+        this.changed = false;
+        await this.controller.cancel(null);
+    }
+    performLaunch = async () => {
+        await this.model.launch();
+        await this.close();
+    }
+    performLaunchDedicated = async () => {
+        await this.model.launchDedicated();
+        await this.close();
+    }
+    async close() {
+        this.changed = false;
+        await this.controller.ok();
+    }
+    cancel = uiCommand2('Cancel', this.performCancel, { cls: "cancel" });
+    launch: IReactiveCommand<void>;
+    launchDedicated: IReactiveCommand<void>;
 }
