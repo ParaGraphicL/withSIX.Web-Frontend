378d3cd0ba42aecd8d7e56872ff8a9762223c23f
Patrick Roza <roza@sixnetworks.net>
2016-09-21 13:14:16 +0200

    Improve server handling

--- src/features/games/servers/sb-server-item.ts
+++ src/features/games/servers/sb-server-item.ts
@@ -1,21 +1,47 @@
-import { IIPEndpoint, GameHelper } from "../../../framework";
+import { GameHelper, IIPEndpoint, IServerInfo, ViewModel, uiCommand2 } from "../../../framework";
+import { GetServer } from "./show";
 
-
-export class SbServerItem {
-  model;
+export class SbServerItem extends ViewModel {
+  model: {
+    name; numPlayers; maxPlayers; missionName; mapName;
+  } = <any>{}
   address: string;
   slug: string;
+  loading: boolean;
+  modelPartial;
+  model2: {
+    info: {
+      name; numPlayers; maxPlayers; mission;
+    }
+  } = <any>{}
+  mods = [];
+
+  refresh = uiCommand2("", () => this.loadModel(this.modelPartial), {icon: "withSIX-icon-Reload"});
+  join = uiCommand2("", async () => alert("TODO"), {icon: "withSIX-icon-Rocket"});
 
   activate(model: { address: IIPEndpoint, gameId: string }) {
+    this.modelPartial = model;
     const w6Cheat = <any> window.w6Cheat;
     const servers = w6Cheat.servers || (w6Cheat.servers = {});
     const gameServers: Map<string, any> = servers[model.gameId];
-    this.model = gameServers.get(this.address = GameHelper.toAddresss(model.address));
+    this.model2 = gameServers.get(this.address = GameHelper.toAddresss(model.address));
     this.slug = `${this.address.replace(/\./g, "-")}/${this.name ? this.name.sluggifyEntityName() : "no-name"}`;
+    this.refresh();
   }
 
-  get name() { return this.model.info.name; };
-  get numPlayers() { return this.model.info.numPlayers; };
-  get maxPlayers() { return this.model.info.maxPlayers; };
-  get mission() { return this.model.info.mission; };
+  get name() { return this.model.name || this.model2.info.name; };
+  get numPlayers() { return this.model.numPlayers || this.model2.info.numPlayers; };
+  get maxPlayers() { return this.model.maxPlayers || this.model2.info.maxPlayers; };
+  get mission() { return this.model.missionName || this.model2.info.mission; };
+  get mapName() { return this.model.mapName; }
+
+  async loadModel(model: IServer) {
+    this.model = await new GetServer(model.gameId, model.address, false).handle(this.mediator);
+    this.loading = false;
+  }
+}
+
+interface IServer {
+  address: IIPEndpoint;
+  gameId: string;
 }
