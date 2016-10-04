86385cb374da150bd336e86bc8d3d68afbfd6243
Patrick Roza <roza@sixnetworks.net>
2016-10-04 14:22:08 +0200

    Cleanup

--- src/features/games/servers/sb-server-item.ts
+++ src/features/games/servers/sb-server-item.ts
@@ -2,35 +2,25 @@ import { GameHelper,  IIPEndpoint, IServerInfo, LaunchAction, LaunchGame, ViewMo
 import { GetServer } from "./server-render-base";
 import { ServerRender } from "./server-render";
 
+interface Info {
+  name; currentPlayers; maxPlayers; mission
+}
+
 export class SbServerItem extends ViewModel {
-  model: {
-    name; numPlayers; maxPlayers; missionName; mapName;
-  } = <any>{}
+  model: Info = <any>{}
   address: string;
   slug: string;
   loading: boolean;
   modelPartial;
   model2: {
-    info: {
-      name; numPlayers; maxPlayers; mission;
-    }
+    info: Info
   } = <any> {};
   mods = [];
 
   refresh = uiCommand2("", () => this.loadModel(this.modelPartial), {icon: "withSIX-icon-Reload"});
   join = uiCommand2("", () => this.launch(), { icon: "withSIX-icon-Rocket" });
 
-  showServer() {
-    return this.dialog.open({model: this.address, viewModel: ServerRender})
-  }
-
-  launch() {
-    const act = new LaunchGame(this.w6.activeGame.id);
-    act.action = LaunchAction.Join;
-    act.serverAddress = this.address;
-    return act.handle(this.mediator);
-  }
-
+  
   activate(model: { address: string, gameId: string }) {
     this.modelPartial = model;
     const w6Cheat = <any> window.w6Cheat;
@@ -41,11 +31,19 @@ export class SbServerItem extends ViewModel {
     this.refresh();
   }
 
+  showServer() { return this.dialog.open({model: this.address, viewModel: ServerRender}) }
+
+  launch() {
+    const act = new LaunchGame(this.w6.activeGame.id);
+    act.action = LaunchAction.Join;
+    act.serverAddress = this.address;
+    return act.handle(this.mediator);
+  }
+
   get name() { return this.model.name || this.model2.info.name; };
-  get numPlayers() { return this.model.numPlayers || this.model2.info.numPlayers; };
+  get currentPlayers() { return this.model.currentPlayers || this.model2.info.currentPlayers; };
   get maxPlayers() { return this.model.maxPlayers || this.model2.info.maxPlayers; };
-  get mission() { return this.model.missionName || this.model2.info.mission; };
-  get mapName() { return this.model.mapName; }
+  get mission() { return this.model.mission || this.model2.info.mission; };
 
   async loadModel(model: IServer) {
     this.model = await new GetServer(model.gameId, model.address, false).handle(this.mediator);
