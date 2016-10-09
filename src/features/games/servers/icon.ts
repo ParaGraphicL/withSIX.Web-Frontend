1e8e98bc8099f8d702078a735b005d29350e9eac
Patrick Roza <roza@sixnetworks.net>
2016-10-09 15:42:32 +0200

    Improve server rendering

---
+++ src/features/games/servers/icon.ts
@@ -0,0 +1,8 @@
+import {
+  bindable
+} from 'aurelia-framework'
+export class Icon {
+  @bindable model;
+  @bindable cls;
+  @bindable title;
+}
--- src/features/games/servers/index2.ts
+++ src/features/games/servers/index2.ts
@@ -1,6 +1,18 @@
-import { ViewModel, Query, DbQuery, IPaginated, handlerFor, SortDirection, IFilter } from '../../../framework';
-import { FilteredBase } from '../../filtered-base';
-import { ServerRender } from './server-render';
+import {
+  ViewModel,
+  Query,
+  DbQuery,
+  IPaginated,
+  handlerFor,
+  SortDirection,
+  IFilter
+} from '../../../framework';
+import {
+  FilteredBase
+} from '../../filtered-base';
+import {
+  ServerRender
+} from './server-render';
 
 interface IServer {
   queryAddress: string;
@@ -16,42 +28,59 @@ interface IServer {
   isDedicated: boolean;
   version: string;
 }
-export class Index extends FilteredBase<IServer> {
-  filters: IFilter<IServer>[] = [
-    {
-      title: "Has Players",
-      name: "hasPlayers",
-      filter: () => true,
-    },
-    {
-      title: "Dedicated",
-      name: "isDedicated",
-      filter: () => true,
-    },
-    {
-      title: "Open",
-      name: "isOpen",
-      filter: () => true,
-    },
-    {
-      title: "Continent",
-      name: "areaLimit",
-      values: [
-        { title: "Europe", value: "EU" },
-        { title: "Norh America", value: "NA" },
-        { title: "South America", value: "SA" },
-        { title: "Oceania", value: "OC" },
-        { title: "Asia", value: "AS" }
-      ],
-      filter: () => true,
-    }
-  ]
-  sort = [
-    { name: "currentPlayers", title: "Players", direction: SortDirection.Desc },
-    { name: "name", title: "Name", direction: SortDirection.Asc },
-    { name: "distance", title: "Distance", direction: SortDirection.Asc },
-    { name: "country", title: "Country", direction: SortDirection.Asc },
-  ]
+export class Index extends FilteredBase < IServer > {
+  static getStandardFilters = () => [{
+    title: "Has Players",
+    name: "hasPlayers",
+    filter: () => true,
+  }, {
+    title: "Dedicated",
+    name: "isDedicated",
+    filter: () => true,
+  }, {
+    title: "Open",
+    name: "isOpen",
+    filter: () => true,
+  }, {
+    title: "Continent",
+    name: "areaLimit",
+    values: [{
+      title: "Europe",
+      value: "EU"
+    }, {
+      title: "Norh America",
+      value: "NA"
+    }, {
+      title: "South America",
+      value: "SA"
+    }, {
+      title: "Oceania",
+      value: "OC"
+    }, {
+      title: "Asia",
+      value: "AS"
+    }],
+    filter: () => true,
+  }]
+  static getStandardSort = () => [{
+    name: "currentPlayers",
+    title: "Players",
+    direction: SortDirection.Desc
+  }, {
+    name: "name",
+    title: "Name",
+    direction: SortDirection.Asc
+  }, {
+    name: "distance",
+    title: "Distance",
+    direction: SortDirection.Asc
+  }, {
+    name: "country",
+    title: "Country",
+    direction: SortDirection.Asc
+  }, ]
+  filters: IFilter < IServer > [] = Index.getStandardFilters();
+  sort = Index.getStandardSort();
   searchFields = ["name"];
   // TODO Custom filters
   getMore(page = 1) {
@@ -59,7 +88,9 @@ export class Index extends FilteredBase<IServer> {
     const search = this.filterInfo.search;
     let filters = undefined;
     if (search.input || this.filterInfo.enabledFilters.length > 0) {
-      const f: { search?: string; minPlayers?: number; isDedicated?: boolean } = {};
+      const f: {
+        search ? : string;minPlayers ? : number;isDedicated ? : boolean
+      } = {};
       if (search.input) f.search = search.input;
       this.filterInfo.enabledFilters.forEach(x => {
         if (x.name === "hasPlayers") f.minPlayers = 1;
@@ -68,36 +99,64 @@ export class Index extends FilteredBase<IServer> {
       filters = f
     }
     const so = this.filterInfo.sortOrder;
-    const sort = so ? { orders: [{ column: so.name, direction: so.direction }] } : undefined;
-    return new GetServers(this.w6.activeGame.id, filters, sort, { page }).handle(this.mediator);
+    const sort = so ? {
+      orders: [{
+        column: so.name,
+        direction: so.direction
+      }]
+    } : undefined;
+    return new GetServers(this.w6.activeGame.id, filters, sort, {
+      page
+    }).handle(this.mediator);
   }
 
-  addPage = async () => {
+  addPage = async() => {
     if (!this.morePagesAvailable) return;
-    const m = <any>this.model;
-    let r = <any> await this.getMore(m.pageNumber + 1);
+    const m = < any > this.model;
+    let r = < any > await this.getMore(m.pageNumber + 1);
     m.total = r.total;
     m.pageNumber = r.pageNumber;
     m.items.push(...r.items);
   }
 
   // adapt to pageModel instead of Breeze
-  get totalPages() { return this.inlineCount / (<any> this.model).pageSize }
-  get inlineCount() { return (<any> this.model).total }
-  get page() { return (<any>this.model).pageNumber }
+  get totalPages() {
+    return this.inlineCount / ( < any > this.model).pageSize
+  }
+  get inlineCount() {
+    return ( < any > this.model).total
+  }
+  get page() {
+    return ( < any > this.model).pageNumber
+  }
 
-    showServer(server: IServer) {
-    return this.dialog.open({model: server.queryAddress, viewModel: ServerRender})
+  showServer(server: IServer) {
+    return this.dialog.open({
+      model: server.queryAddress,
+      viewModel: ServerRender
+    })
   }
 }
 
-class GetServers extends Query<IPaginated<IServer>> { 
-  constructor(public gameId: string, public filter?: { name?: string; currentPlayers?: number; isDedicated?: boolean}, public order?: { orders: { column: string; direction: number }[] }, public pageInfo?: { page: number }) { super(); }
+class GetServers extends Query < IPaginated < IServer >> {
+  constructor(public gameId: string, public filter ? : {
+    name ? : string;currentPlayers ? : number;isDedicated ? : boolean
+  }, public order ? : {
+    orders: {
+      column: string;direction: number
+    }[]
+  }, public pageInfo ? : {
+    page: number
+  }) {
+    super();
+  }
 }
 
 @handlerFor(GetServers)
-class GetServersHandler extends DbQuery<GetServers, IPaginated<IServer>> {
+class GetServersHandler extends DbQuery < GetServers, IPaginated < IServer >> {
   handle(request: GetServers) {
-    return this.context.getCustom("servers", { params: request })
+    return this.context.getCustom("servers", {
+      params: request
+    })
   }
 }
