e2ba0ec286c207507c77161c828dfa99bc6686d3
Patrick Roza <roza@sixnetworks.net>
2016-10-09 17:33:10 +0200

    Improve server info

--- src/features/games/servers/icon.ts
+++ src/features/games/servers/icon.ts
@@ -4,5 +4,15 @@ import {
 export class Icon {
   @bindable model;
   @bindable cls;
-  @bindable title;
+  @bindable optionalTitle;
+}
+
+
+export class DistanceValueConverter {
+  toView = (distance ? : number) => {
+    if (!distance || distance > (2000 * 1000))
+      return "withSIX-icon-Connection-Low";
+    if (distance < (500 * 1000)) return "withSIX-icon-Connection-High"
+    return "withSIX-icon-Connection-Med"
+  }
 }
