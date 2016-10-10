00954583c5fe470606b7d9d091d7ce2a15417c8a
Patrick Roza <roza@sixnetworks.net>
2016-10-10 12:17:02 +0200

    Impove linking

--- src/features/games/servers/index2.ts
+++ src/features/games/servers/index2.ts
@@ -1,4 +1,12 @@
+import { activationStrategy } from 'aurelia-router';
+
 export class Index2 {
   params;
   activate(params) { this.params = params }
+
+    determineActivationStrategy() {
+    return activationStrategy.replace; //replace the viewmodel with a new instance
+    // or activationStrategy.invokeLifecycle to invoke router lifecycle methods on the existing VM
+    // or activationStrategy.noChange to explicitly use the default behavior
+  }
 }
