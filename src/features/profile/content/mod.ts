11d68c2acf5e2a5126a182ba130d5c8cd410f9c0
Patrick Roza <roza@sixnetworks.net>
2016-10-10 11:18:48 +0200

    Add mod filters

--- src/features/profile/content/mod.ts
+++ src/features/profile/content/mod.ts
@@ -1,5 +1,5 @@
-import {ContentViewModel} from './base';
-import {IMod, uiCommand2, MenuItem} from '../../../framework';
+import { ContentViewModel } from './base';
+import { IMod, uiCommand2, MenuItem } from '../../../framework';
 
 export class Mod extends ContentViewModel<IMod> {
   icon = "withSIX-icon-Nav-Mod";
@@ -10,5 +10,10 @@ export class Mod extends ContentViewModel<IMod> {
   setupMenuItems() {
     super.setupMenuItems();
     this.setupAddToBasket();
+    if (this.features.serverBrowser) this.topMenuActions.push(new MenuItem(this.findServers));
   }
+
+  findServers = uiCommand2("Find servers with this",
+    async () => this.w6.navigate(`/p/${this.w6.activeGame.slug}/servers2/?modId=${this.model.id}`),
+    {icon: "withSIX-icon-Nav-Server"})
 }
