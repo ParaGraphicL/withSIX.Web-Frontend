9d80d8abed645f731e912a5e2fcf59a5a69201e1
Patrick Roza <roza@sixnetworks.net>
2016-10-20 17:15:17 +0200

    Prepare UI for server browser adjustment

--- src/features/games/servers/index2.ts
+++ src/features/games/servers/index2.ts
@@ -1,10 +1,20 @@
 import { activationStrategy } from 'aurelia-router';
 
-export class Index2 {
-  params;
-  activate(params) { this.params = params }
+import { ViewModel } from "../../../framework";
 
-    determineActivationStrategy() {
+export class Index2 extends ViewModel {
+  params;
+  activate(params) {
+    this.params = params;
+    this.w6.classes = 'v3';
+  }
+
+  deactivate() {
+    super.deactivate();
+    this.w6.classes = '';
+  }
+
+  determineActivationStrategy() {
     return activationStrategy.replace; //replace the viewmodel with a new instance
     // or activationStrategy.invokeLifecycle to invoke router lifecycle methods on the existing VM
     // or activationStrategy.noChange to explicitly use the default behavior
