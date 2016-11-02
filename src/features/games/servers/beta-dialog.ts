7351f04964482abfff29d5e2b5bbef5b88cabbd3
Patrick Roza <roza@sixnetworks.net>
2016-11-02 13:53:03 +0100

    Implement BetaDialog

---
+++ src/features/games/servers/beta-dialog.ts
@@ -0,0 +1,5 @@
+import { Dialog, uiCommand2 } from '../../../framework';
+export class BetaDialog extends Dialog<{ dontShowAgain?: boolean }> {
+  model = { dontShowAgain: false };
+  ok = uiCommand2("Let's start", () => this.controller.close(this.model.dontShowAgain));
+}
