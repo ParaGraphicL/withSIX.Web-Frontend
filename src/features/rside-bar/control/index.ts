0acc88ee7eba557a03460d0c9ebeccbf5fb8abd7
Patrick Roza <roza@sixnetworks.net>
2016-11-23 11:20:22 +0100

    Merge Players, Stats and Status into Control

---
+++ src/features/rside-bar/control/index.ts
@@ -0,0 +1,18 @@
+import { ITabModel, ServerTab } from "../rside-bar";
+import { uiCommand2 } from "../../../framework";
+
+interface IStatusTab extends ITabModel<any> { }
+
+export class Index extends ServerTab<IStatusTab> {
+  start = uiCommand2("Start", async () => { });
+  stop = uiCommand2("Stop", async () => { });
+  restart = uiCommand2("Restart", async () => { });
+  prepare = uiCommand2("Prepare content and configs", async () => { });
+  status = "Stopped";
+  lock = uiCommand2("Lock", async () => { });
+  unlock = uiCommand2("Unlock", async () => { });
+  players = [
+    { name: "Player X" },
+    { name: "Player Y" },
+  ]
+}
--- src/features/rside-bar/rside-bar.ts
+++ src/features/rside-bar/rside-bar.ts
@@ -8,9 +8,7 @@ export class RsideBar extends ViewModel {
     { header: "Settings", name: "settings", icon: "icon withSIX-icon-Settings", viewModel: `${RsideBar.root}settings/index`, next: (tab: IAwesomeTab) => this.next(tab) },
     { header: "Mods", name: "mods", icon: "icon withSIX-icon-Nav-Mod", viewModel: `${RsideBar.root}mods/index`, next: (tab: IAwesomeTab) => this.next(tab) },
     { header: "Missions", name: "missions", icon: "icon withSIX-icon-Nav-Mission", viewModel: `${RsideBar.root}missions/index`, next: (tab: IAwesomeTab) => this.next(tab) },
-    { header: "Players", name: "players", location: "end", icon: "icon withSIX-icon-User-Community", viewModel: `${RsideBar.root}players/index`, next: (tab: IAwesomeTab) => this.next(tab) },
-    { header: "Stats", name: "stats", location: "end", icon: "icon withSIX-icon-Share-Lines", viewModel: `${RsideBar.root}stats/index`, next: (tab: IAwesomeTab) => this.next(tab) },
-    { header: "Status", name: "status", location: "end", icon: "icon withSIX-icon-System-Remote", viewModel: `${RsideBar.root}status/index`, next: (tab: IAwesomeTab) => this.next(tab) },
+    { header: "Control", name: "control", location: "end", icon: "icon withSIX-icon-System-Remote", viewModel: `${RsideBar.root}control/index`, next: (tab: IAwesomeTab) => this.next(tab) },
   ];
 
   get validSetup() { return this.tabs[0].isValid; }
