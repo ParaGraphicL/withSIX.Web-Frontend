2f88439e864b6e5b8ac6f9540143c81c82a2a1d9
Patrick Roza <roza@sixnetworks.net>
2016-12-09 21:43:10 +0100

    Load actual data

--- src/features/user-profile/servers/index.ts
+++ src/features/user-profile/servers/index.ts
@@ -1,20 +1,42 @@
-export class Index {
+import { gql, handlerFor, Query, ViewModel } from "../../../framework";
+import { ServerHandler } from "../../rside-bar/control/actions/base";
+
+export class Index extends ViewModel {
   data;
   get user() { return this.data; }
   get servers() { return this.data.servers; }
 
-  activate({ slug }) {
+  async activate({ slug }) {
+    const { data } = await new GetUserServers(slug).handle(this.mediator);
     this.data = {
       displayName: "Testaccount1",
       slug: "Testaccount1",
       servers: {
-        edges: [{
-          id: "POKokpok32232232==",
-          name: "Server 1: Changed name",
-          slug: "Server-1",
-        }],
-        totalCount: 1,
+        edges: data.managedServersByUser,
+        totalCount: data.managedServersByUser.length,
       }
     };
+  }
+}
+
+export class GetUserServers extends Query<{ data }> {
+  constructor(public userSlug: string) { super(); }
+}
+
+@handlerFor(GetUserServers)
+export class GetUserServersHandler extends ServerHandler<GetUserServers, { data }> {
+  handle(req: GetUserServers) {
+    return this.gql.ac.query({
+      query: gql`
+      query ManagedServers($user: String!) {
+          managedServersByUser(user: $user) {
+          id
+          name
+          slug
+        }
+      }
+    `, variables: { user: req.userSlug },
+      forceFetch: true, // todo
+    });
   }
 }
