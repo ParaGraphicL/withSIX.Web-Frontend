996c98e80632a050c1b0064525a0cd49ab001c27
Patrick Roza <roza@sixnetworks.net>
2016-12-23 13:50:16 +0100

    Enable signup post

--- src/services/w6api/server-client.ts
+++ src/services/w6api/server-client.ts
@@ -1,4 +1,4 @@
-import { ServersApi } from "./servers-api";
+import { ServersApi, ServerSignupApi } from "./servers-api";
 import { W6Context } from "../w6context";
 import { inject } from "aurelia-framework";
 import { ServerFileUploader } from "./server-file-uploader";
@@ -7,7 +7,8 @@ export interface IServerClient {
   servers: ServersApi;
 }
 
-@inject(ServersApi, ServerFileUploader)
+@inject(ServersApi, ServerSignupApi, ServerFileUploader)
 export class ServerClient implements IServerClient {
-  constructor(public servers: ServersApi, public uploader: ServerFileUploader) { }
+  constructor(public servers: ServersApi, public signup: ServerSignupApi,
+    public uploader: ServerFileUploader) { }
 }
